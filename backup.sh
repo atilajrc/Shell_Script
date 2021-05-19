59ad3e3381f6fde2ef48a3399e97fe66b032f92c
atilajrc <atilajrc@gmail.com>
2021-05-19 14:34:18 -0300

    Shell
    Signed-off-by: atilajrc <atilajrc@gmail.com>

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 umount /mnt/Backup/
-mount -t cifs -o user=importec,password=importec,vers=1.0 //192.168.1.5/backup/backup /mnt/Backup/
+mount -t cifs -o user=xxx,password=xxx,vers=1.0 //192.168.1.5/backup/backup /mnt/Backup/
 rsync -razvp --progress /mnt/Backup/ /mnt/Files/Backup/Dropbox/ENTERNESS/
 umount /mnt/Backup/
 find /mnt/Files/Backup/Dropbox/GEWEB ! -mtime -30 | xargs rm -rf
-wget -rnd -c -P /mnt/Files/Backup/Dropbox/GEWEB/ -A 7z http://192.168.1.2/bkp/4301/diario/ --http-user=importec --http-passwd=importec
-wget -rnd -c -P /mnt/Files/Backup/Dropbox/GEWEB_MENSAL/ -A 7z http://192.168.1.2/bkp/4301/mensal/ --http-user=importec --http-passwd=importec
+wget -rnd -c -P /mnt/Files/Backup/Dropbox/GEWEB/ -A 7z http://192.168.1.2/bkp/4301/diario/ --http-user=xxx --http-passwd=xxx
+wget -rnd -c -P /mnt/Files/Backup/Dropbox/GEWEB_MENSAL/ -A 7z http://192.168.1.2/bkp/4301/mensal/ --http-user=xxx --http-passwd=xxx
 umount /mnt/Backup/
-mount -t cifs -o user=Administrator,password=CRM@2017##,vers=1.0 //192.168.1.4/_BackupBanco /mnt/Backup/
+mount -t cifs -o user=xxx,password=xxx,vers=1.0 //192.168.1.4/_BackupBanco /mnt/Backup/
 rsync -razvp --progress /mnt/Backup/ /mnt/Files/Backup/Dropbox/CRM/
 umount /mnt/Backup/
--- teste_rede.sh
+++ teste_rede.sh
@@ -8,7 +8,7 @@ echo
 echo ping -c 10 192.168.1.2
 ping -c 10 192.168.1.2
 echo
-echo wget http://192.168.1.2/bkp/4301/diario/importec-11.7z --http-user=importec --http-passwd=importec
-wget http://192.168.1.2/bkp/4301/diario/importec-11.7z --http-user=importec --http-passwd=importec
+echo wget http://192.168.1.2/bkp/4301/diario/importec-11.7z --http-user=xxx --http-passwd=xxx
+wget http://192.168.1.2/bkp/4301/diario/importec-11.7z --http-user=xxx --http-passwd=xxx
 echo rm importec-11.7z
 rm importec-11.7z
